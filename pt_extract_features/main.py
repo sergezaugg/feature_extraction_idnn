41f99663b6a934b08e5d826cb9135d7712775ec8
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-11 08:57:03 +0200

    ok - all functionality now in the dedicated class

--- pt_extract_features/main.py
+++ pt_extract_features/main.py
@@ -1,10 +1,10 @@
 #--------------------             
 # Author : Serge Zaugg
-# Description : A short script to illustrate usage of pt_extract_features.utils_ml.FeatureExtractor and dim_reduce
+# Description : A short script to illustrate usage of pt_extract_features.utils_ml.FeatureExtractor
 #--------------------
 
 import torch
-from pt_extract_features.utils_ml import FeatureExtractor, dim_reduce
+from pt_extract_features.utils_ml import FeatureExtractor
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.cuda.is_available()
 
@@ -12,22 +12,20 @@ torch.cuda.is_available()
 image_path = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
 featu_path = "./extracted_features"
 
-# feature extraction
+# feature extraction and dim reduction
 fe = FeatureExtractor(model_tag = "ResNet50")
 fe.eval_nodes
 fe.create("layer1.2.conv3")
-fe.extract(image_path, featu_path, freq_pool = 4, batch_size = 16, n_batches = 5)
+fe.extract(image_path, featu_path, freq_pool = 4, batch_size = 16, n_batches = 25)
 fe.save_full_features() # only needed if fe.extract was interrupted
 fe.reduce_dimension(n_neigh = 10, reduced_dim = 16)
 
-
+# explore resulting arrays
+print(fe.N.shape, fe.X.shape, fe.X_red.shape, fe.X_2D.shape,)
 fe.plot_full_features(n=80).show()
 fe.plot_reduced_features(n=80).show()
 
 
-fe.out_name
-fe.out_name_reduced
-print(fe.N.shape, fe.X.shape, fe.X_red.shape, fe.X_2D.shape,)
 
 
  
