2881d9e51f7aa6a5f61c7680f59661a27fd60e5a
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-16 16:38:31 +0200

    lets finish this .... ongoing work

--- pt_extract_features/utils_ml.py
+++ pt_extract_features/utils_ml.py
@@ -46,75 +46,82 @@ class ImageDataset(Dataset):
         return (len(self.all_img_files))
     
 
-def load_pretraind_model(model_tag):
-    """
-    """
-    if model_tag == "ResNet50":
-        from torchvision.models import resnet50, ResNet50_Weights
-        weights = ResNet50_Weights.IMAGENET1K_V2
-        model = resnet50(weights=weights)
-    elif model_tag == "DenseNet121":
-        from torchvision.models import densenet121, DenseNet121_Weights 
-        weights = DenseNet121_Weights.IMAGENET1K_V1
-        model = densenet121(weights=weights)
-    elif model_tag == "MobileNet_V3_Large":
-        from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
-        weights = MobileNet_V3_Large_Weights.IMAGENET1K_V2
-        model = mobilenet_v3_large(weights=weights)
-    elif model_tag == "MobileNet_randinit":
-        from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
-        weights = MobileNet_V3_Large_Weights.IMAGENET1K_V2
-        model = mobilenet_v3_large(weights=None)
-    elif model_tag == "vgg16":
-        from torchvision.models import vgg16, VGG16_Weights
-        weights = VGG16_Weights.IMAGENET1K_V1
-        model = vgg16(weights=weights)
-    # Transformers 
-    elif model_tag == "Vit_b_16":
-        from torchvision.models import vit_b_16, ViT_B_16_Weights
-        weights = ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1
-        model = vit_b_16(weights=weights)
-    elif model_tag == "MaxVit_T":
-        from torchvision.models import maxvit_t, MaxVit_T_Weights  
-        weights = MaxVit_T_Weights.IMAGENET1K_V1     
-        model = maxvit_t(weights=weights)  
-    elif model_tag == "Swin_S":
-        from torchvision.models import swin_s, Swin_S_Weights
-        weights = Swin_S_Weights.IMAGENET1K_V1
-        model = swin_s(weights=weights)
-    else:
-        print("not a valid model_tag")
-    return(model, weights)    
+  
 
 
-def dim_reduce(X, n_neigh, n_dims_red):
-    """
-    UMAP dim reduction for clustering
-    """
-    scaler = StandardScaler()
-    reducer = umap.UMAP(
-        n_neighbors = n_neigh, 
-        n_components = n_dims_red, 
-        metric = 'euclidean',
-        n_jobs = -1
-        )
-    X_scaled = scaler.fit_transform(X)
-    X_trans = reducer.fit_transform(X_scaled)
-    X_out = scaler.fit_transform(X_trans)
-    return(X_out)
 
 
 class FeatureExtractor:
     """
-    Description: Create a PyTorch feature extractor from a pretrained model 
     """
+
+    def _load_pretraind_model(self, model_tag):
+        """
+        """
+        if model_tag == "ResNet50":
+            from torchvision.models import resnet50, ResNet50_Weights
+            weights = ResNet50_Weights.IMAGENET1K_V2
+            model = resnet50(weights=weights)
+        elif model_tag == "DenseNet121":
+            from torchvision.models import densenet121, DenseNet121_Weights 
+            weights = DenseNet121_Weights.IMAGENET1K_V1
+            model = densenet121(weights=weights)
+        elif model_tag == "MobileNet_V3_Large":
+            from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
+            weights = MobileNet_V3_Large_Weights.IMAGENET1K_V2
+            model = mobilenet_v3_large(weights=weights)
+        elif model_tag == "MobileNet_randinit":
+            from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
+            weights = MobileNet_V3_Large_Weights.IMAGENET1K_V2
+            model = mobilenet_v3_large(weights=None)
+        elif model_tag == "vgg16":
+            from torchvision.models import vgg16, VGG16_Weights
+            weights = VGG16_Weights.IMAGENET1K_V1
+            model = vgg16(weights=weights)
+        # Transformers 
+        elif model_tag == "Vit_b_16":
+            from torchvision.models import vit_b_16, ViT_B_16_Weights
+            weights = ViT_B_16_Weights.IMAGENET1K_SWAG_E2E_V1
+            model = vit_b_16(weights=weights)
+        elif model_tag == "MaxVit_T":
+            from torchvision.models import maxvit_t, MaxVit_T_Weights  
+            weights = MaxVit_T_Weights.IMAGENET1K_V1     
+            model = maxvit_t(weights=weights)  
+        elif model_tag == "Swin_S":
+            from torchvision.models import swin_s, Swin_S_Weights
+            weights = Swin_S_Weights.IMAGENET1K_V1
+            model = swin_s(weights=weights)
+        else:
+            print("not a valid model_tag")
+        return(model, weights)  
+
+
+
+    def _dim_reduce(self, X, n_neigh, n_dims_red):
+        """
+        """
+        scaler = StandardScaler()
+        reducer = umap.UMAP(
+            n_neighbors = n_neigh, 
+            n_components = n_dims_red, 
+            metric = 'euclidean',
+            n_jobs = -1
+            )
+        X_scaled = scaler.fit_transform(X)
+        X_trans = reducer.fit_transform(X_scaled)
+        X_out = scaler.fit_transform(X_trans)
+        return(X_out)
+    
+
+
+
 
     def __init__(self, model_tag):
         """
-        model_tag (str) : A model name as accepted by load_pretraind_model()
+        
         """
         self.model_tag = model_tag
-        self.model, weights = load_pretraind_model(model_tag)
+        self.model, weights = self._load_pretraind_model(model_tag)
         self.preprocessor = weights.transforms()
         self.train_nodes, self.eval_nodes = get_graph_node_names(self.model)
 
@@ -215,8 +222,8 @@ class FeatureExtractor:
         X = npzfile['X']
         N = npzfile['N']
         # make 2d feats needed for plot 
-        self.X_2D  = dim_reduce(X, n_neigh, 2)
-        self.X_red = dim_reduce(X, n_neigh, reduced_dim)
+        self.X_2D  = self._dim_reduce(X, n_neigh, 2)
+        self.X_red = self._dim_reduce(X, n_neigh, reduced_dim)
         # save as npz
         tag_dim_red = "dimred_" + str(reduced_dim) + "_neigh_" + str(n_neigh) + "_"
         file_name_out = '_'.join(file_name_in.split('_')[0:2]) + '_' + tag_dim_red + '_'.join(file_name_in.split('_')[4:])
--- readme.md
+++ readme.md
@@ -1,26 +1,27 @@
 
 # Extract features from spectrograms with pre-trained image DNNs and dim-reduction 
 
-### Overview
 
+### Table of Contents
+* [Overview](#Overview)
+* [Intallation](#Intallation)
+* [Configuration](#Configuration)
+* [Usage](#Usage)
+* [ML details](#ML-details)
+
+
+### Overview
 * Intended for acoustic recordings from [xeno-canto](https://xeno-canto.org/)
-* Download and acoustic data preparation can be performed with [this tool](https://github.com/sergezaugg/xeno_canto_organizer)  
-    * In a nutshell: MP3 converted to WAVE, resampled, segmented, transformed to spectrograms, and stored as RGB images.
-* Images can then be processed with the **FeatureExtractor** Class provided here
-    * First, extract array features from inner layers of pre-trained image DNNs
-    * Second, transform arrays to rather long linear features
-    * Third, dim reduce the linear features with UMAP to shorter feature vectors
-    * Full and reduced-dim features as stored as NPZ files
+* Download of mp3 and conversion to spectrograms as RGB images can be performed with [this tool](https://github.com/sergezaugg/xeno_canto_organizer)  
+* Images can then be processed with the **FeatureExtractor** Class provided in [pt_extract_features/utils_ml.py](pt_extract_features/utils_ml.py)  pt_extract_features/utils_ml.py
+* First, extract array features from inner layers of pre-trained image DNNs
+* Second, transform arrays to rather long linear features
+* Third, dim reduce the linear features with UMAP to shorter feature vectors
+* Full and reduced-dim features as stored as NPZ files
 * NPZ files can then stored on a Kaggle dataset [(example)](https://www.kaggle.com/datasets/sezaugg/spectrogram-clustering-01) where the [frontend](https://spectrogram-image-clustering.streamlit.app/) will fetch them.
 *  [Link to frontend](https://spectrogram-image-clustering.streamlit.app/) and its [Github repo](https://github.com/sergezaugg/spectrogram_image_clustering)  
 
-### Usage
-* Code is in this subdir ```./pt_extract_features```
-* Main functionality called via a single class **FeatureExtractor** defined is in ```utils_ml.py```
-* Short example: ```main.py```
-* Full example: ```history.py```
-
-### Dependencies / Intallation
+### Intallation
 * Developed under Python 3.12.8
 * Make a fresh venv!
 * ```pip install -r requirements.txt```
@@ -29,6 +30,14 @@
 * ```pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126```
 * If other CUDA version needed, check instructions here https://pytorch.org/get-started/locally
 
-### ML details
-![](pics/spectro_imDNN_data_flow.png)
+### Configuration
 
+### Usage
+* Code is in this subdir ```./pt_extract_features```
+* Main functionality called via a single class **FeatureExtractor** defined is in ```utils_ml.py```
+* Short example: ```main.py```
+* Full example: ```history.py```
+
+
+### ML details
+<img src="pics/spectro_imDNN_data_flow.png" alt="Example image" width="600"/>
