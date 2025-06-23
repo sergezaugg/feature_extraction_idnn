ef4056d95320f6fd5fbad331f3f0f03a7c775c7b
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-23 13:12:59 +0200

    misc

--- main_batch.py
+++ main_batch.py
@@ -5,6 +5,7 @@
 # Usage example: python main_batch.py -i "D:/xc_real_projects/xc_sw_europe/xc_spectrograms" -n 21
 # spec example:  python main_batch.py -i "D:/xc_real_projects/xc_sw_europe/xc_spectrograms" -n 1000000
 # spec example:  python main_batch.py -i "D:/xc_real_projects/xc_parus_01/xc_spectrograms" -n 1000000
+# spec example:  python main_batch.py -i "D:/xc_real_projects/xc_corvus_corax/xc_spectrograms" -n 1000000
 # Usage example: python main_batch.py -i "./dev_data/images" -n 31
 #--------------------
 
@@ -58,30 +59,29 @@ fe.create("layer3.5.conv3")
 fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
 [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
-# new 
 fe = FeatureExtractor(model_tag = "ResNet50")
 fe.eval_nodes
 fe.create("layer4.2.conv3")
 fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 0)
 [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
-fe = FeatureExtractor(model_tag = "DenseNet121")
-fe.eval_nodes
-fe.create("features.denseblock3")
-fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
-[fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+# fe = FeatureExtractor(model_tag = "DenseNet121")
+# fe.eval_nodes
+# fe.create("features.denseblock3")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
+# [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
-fe = FeatureExtractor(model_tag = "vgg16")
-fe.eval_nodes
-fe.create("features.28")
-fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
-[fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+# fe = FeatureExtractor(model_tag = "vgg16")
+# fe.eval_nodes
+# fe.create("features.28")
+# fe.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
+# [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
-fe = FeatureExtractor(model_tag = "MaxVit_T")
-fe.eval_nodes
-fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
-fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = n_batches, ecut = 1)
-[fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+# fe = FeatureExtractor(model_tag = "MaxVit_T")
+# fe.eval_nodes
+# fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
+# fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = n_batches, ecut = 1)
+# [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
 
 
