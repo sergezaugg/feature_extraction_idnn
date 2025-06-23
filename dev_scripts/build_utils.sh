4aa21a623bf8bb9db8e186c0c5293514ce3a1e64
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-23 18:03:51 +0200

    try to deploy as whl

--- dev_scripts/build_utils.sh
+++ dev_scripts/build_utils.sh
@@ -8,11 +8,9 @@ python -m build
 
 
 
-
-
 pip uninstall fe_idnn 
 
-pip install --upgrade dist/fe_idnn-0.0.1.2-py3-none-any.whl
+pip install --upgrade dist/fe_idnn-0.0.2-py3-none-any.whl
 
 pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
 
