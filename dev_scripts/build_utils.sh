3e526bde114950b77ee2e53aaf391f2b72938e57
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-24 08:54:36 +0200

    pimped up the readme

--- dev_scripts/build_utils.sh
+++ dev_scripts/build_utils.sh
@@ -10,16 +10,14 @@ python -m build
 
 pip uninstall fe_idnn 
 
-pip install --upgrade dist/fe_idnn-0.0.2-py3-none-any.whl
+python -m venv ./
+
+pip install --upgrade dist/fe_idnn-0.0.6-py3-none-any.whl
 
 pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
 
 
 
 
-
-
-# numpy.reshape(a, newshape, order='C')
-# numpy.reshape(a, /, shape=None, order='C', *
 
 
