93129153211a869e8ed97006a055385a8d91e5f5
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-26 11:14:59 +0200

    restructured all dev items

--- dev/dev_scripts/dev_script_01.py
+++ dev/dev_scripts/dev_script_01.py
@@ -4,9 +4,7 @@
 # --------------
 
 import torch
-# devel
 from src.fe_idnn import IDNN_extractor
-
 # # usage (must 'pip install' first)
 # from fe_idnn import IDNN_extractor
 
@@ -14,8 +12,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.cuda.is_available()
 
 # set path   
-image_source_path = "./dev_data/images"
-feature_save_path = "C:/Users/sezau/Downloads"
+image_source_path = "./dev/dev_data/images"
+feature_save_path = "./dev/dev_outp"
 
 # feature extraction and dim reduction
 fe = IDNN_extractor(model_tag = "ResNet50")
@@ -27,7 +25,7 @@ fe.extractor
 
 fe.extract(image_path = image_source_path, fe_save_path = feature_save_path,  freq_pool = 4, batch_size = 16, n_batches = 2, ecut = 1)
 fe.save_full_features() # only needed if fe.extract was interrupted
-fe.reduce_dimension(n_neigh = 10, reduced_dim = 10)
+fe.reduce_dimension(n_neigh = 10, reduced_dim = 12)
 # explore resulting arrays
 print(fe.N.shape, fe.X.shape, fe.X_red.shape, fe.X_2D.shape,)
 
--- sample_code.py
+++ sample_code.py
@@ -8,8 +8,10 @@ from fe_idnn import IDNN_extractor
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# set path   
-image_source_path = "./dev_data/images"
+# set paths  
+image_source_path = "./dev/dev_data/images"
+feature_save_path = "./dev/dev_outp"
+
 # initialize instance of feature extraction and dim reduction
 fe = IDNN_extractor(model_tag = "ResNet50")
 # (optional) check model architecture
@@ -18,7 +20,7 @@ fe.eval_nodes
 # create the feature extractor
 fe.create("layer3.5.conv3")
 # extract features from images
-fe.extract(image_path = image_source_path, fe_save_path = "C:/Users/sezau/Downloads",  freq_pool = 4, batch_size = 16, n_batches = 2, ecut = 1)
+fe.extract(image_path = image_source_path, fe_save_path = feature_save_path,  freq_pool = 4, batch_size = 16, n_batches = 2, ecut = 1)
 # only needed if fe.extract was interrupted:
 fe.save_full_features() 
 # apply UMAP to reduce dim
